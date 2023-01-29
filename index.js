ba2533e53111972036a28f4b405276c62ccd3da3
Bhrugu <knowbhrugu14@gmail.com>
2023-01-30 02:02:04 +0530

    ccc

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.use(cors());
 
 const data = {
